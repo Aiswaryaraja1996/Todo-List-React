587a0e75bff79b4300b32ba43088e005057c5707
Aiswaryaraja1996 <63009970+Aiswaryaraja1996@users.noreply.github.com>
2021-11-18 12:17:12 +0530

    todo

--- src/components/Todo/TodoInput.jsx
+++ src/components/Todo/TodoInput.jsx
@@ -4,32 +4,51 @@ import { PlusCircleFill } from "react-bootstrap-icons";
 import style from "./TodoInput.module.css";
 
 const TodoInput = ({ onAddTask }) => {
-  const [task, setTask] = useState({});
+  const [task, setTask] = useState({
+    title: "",
+    description: "",
+  });
 
-  const onAdd = () => {
+  const onAdd = (e) => {
+    e.preventDefault();
     onAddTask(task);
     document.getElementById("title").value = null;
     document.getElementById("desc").value = null;
   };
 
+  const handleChange = (e) => {
+    setTask({...task,[e.target.name]:e.target.value});
+  }
+  const btnStyle = {
+    border: 0,
+    background: "none"
+  };
+
   return (
-    <div className={style.inputForm}>
+    <form className={style.inputForm} onSubmit={onAdd}>
       <h3>T O D O</h3>
       <input
         id="title"
         type="text"
         placeholder="Title"
-        onChange={(e) => setTask({ ...task, title: e.target.value })}
+        onChange={handleChange}
+        value={task.title}
+        name="title"
       ></input>
       <input
         id="desc"
         type="text"
         placeholder="Description"
-        onChange={(e) => setTask({ ...task, description: e.target.value })}
+        onChange={handleChange}
+        value={task.description}
+        name="description"
       ></input>
 
-      <PlusCircleFill onClick={onAdd} color="#43B0F1" size={42} />
-    </div>
+      {/* <PlusCircleFill type="submit" onClick={onAdd} color="#43B0F1" size={42} /> */}
+      <button type="submit" style={btnStyle}>
+      <PlusCircleFill  color="#43B0F1" size={42} />
+      </button>
+    </form>
   );
 };
 
